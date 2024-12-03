3ccaf595de617f64607a9fc8c541e7eab9b0538e
Satriooo <satriocahyo93@gmail.com>
2024-12-03 23:43:45 +0700

    Update HomeFragment & Add Icon Assets.

--- app/src/main/java/com/exal/grocerease/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/exal/grocerease/view/fragment/HomeFragment.kt
@@ -1,23 +1,28 @@
 package com.exal.grocerease.view.fragment
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.exal.grocerease.databinding.FragmentHomeBinding
 import com.exal.grocerease.helper.rupiahFormatter
+import com.exal.grocerease.view.adapter.ExpensesAdapter
 import com.exal.grocerease.viewmodel.HomeViewModel
-import com.exal.grocerease.R
-import com.exal.grocerease.view.adapter.RecentlyAdapter
+import com.exal.grocerease.helper.Resource
+import com.exal.grocerease.view.activity.CreateListActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: HomeViewModel by viewModels()
+    private val homeViewModel: HomeViewModel by viewModels()
 
     private var totalPrice = 0
     private var totalItem = 0
@@ -32,15 +37,46 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecyclerView()
 
-        // Tampilkan total price dan total items ke UI
+        val adapter = ExpensesAdapter()
+        binding.rvExpense.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvExpense.adapter = adapter
+
+        homeViewModel.getExpenseList()
+
+        homeViewModel.expenses.observe(viewLifecycleOwner, { resource ->
+            when (resource) {
+                is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is Resource.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    resource.data?.data?.lists?.take(5).let { expenseList ->
+                        adapter.submitList(expenseList)
+                    }
+                    resource.data?.data?.lists?.take(5)?.forEach { expense ->
+                        totalPrice += expense?.totalExpenses?.toDouble()?.toInt() ?: 0
+                        binding.total.text = rupiahFormatter(totalPrice)
+                        totalItem += expense?.totalItems ?: 0
+                        binding.items.text = "$totalItem Items"
+                    }
+                }
+                is Resource.Error -> {
+                    binding.progressBar.visibility = View.GONE
+                    resource.message?.let { message ->
+                        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+
         binding.total.text = rupiahFormatter(totalPrice)
         binding.items.text = "$totalItem Items"
-    }
 
-    private fun setupRecyclerView() {
-
+        binding.floatingActionButton.setOnClickListener {
+            val intent = Intent(requireContext(), CreateListActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
@@ -48,10 +84,3 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
-
-data class Item(
-    val itemImage: Int,
-    val itemPrice: Int,
-    val itemTotal: Int,
-    val itemDate: String
-)
