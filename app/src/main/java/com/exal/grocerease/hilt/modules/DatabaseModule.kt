f56d2d881f2abb1485e7938f14d817e643c70888
dontkeep <alsdiaryy@gmail.com>
2024-12-09 17:17:35 +0700

    Added : - some adjustment at DatabaseModule ToDo : - AppSettings (language)

--- app/src/main/java/com/exal/grocerease/hilt/modules/DatabaseModule.kt
+++ app/src/main/java/com/exal/grocerease/hilt/modules/DatabaseModule.kt
@@ -1,5 +1,33 @@
 package com.exal.grocerease.hilt.modules
 
+import android.content.Context
+import com.exal.grocerease.model.db.AppDatabase
+import com.exal.grocerease.model.db.dao.ListDao
+import com.exal.grocerease.model.db.dao.RemoteKeysDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
-    // hapus ae ntar
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getDatabase(context)
+    }
+
+    @Provides
+    fun provideListDao(database: AppDatabase): ListDao {
+        return database.listDao()
+    }
+
+    @Provides
+    fun provideRemoteKeysDao(database: AppDatabase): RemoteKeysDao {
+        return database.remoteKeysDao()
+    }
 }
