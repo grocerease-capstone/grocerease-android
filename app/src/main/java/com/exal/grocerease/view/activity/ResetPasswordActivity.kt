2c61bed63acd21bd08fc07bc883f342047ea7462
Satriooo <satriocahyo93@gmail.com>
2024-12-10 16:52:20 +0700

    Add layout ResetPasswordActivity.

--- app/src/main/java/com/exal/grocerease/view/activity/ResetPasswordActivity.kt
+++ app/src/main/java/com/exal/grocerease/view/activity/ResetPasswordActivity.kt
@@ -1,24 +1,23 @@
 package com.exal.grocerease.view.activity
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.exal.grocerease.R
-import com.exal.grocerease.databinding.ActivityAccountSettingsBinding
-import com.exal.grocerease.viewmodel.AccountSettingsViewModel
+import com.exal.grocerease.databinding.ActivityResetPasswordBinding
+import com.exal.grocerease.viewmodel.ResetPasswordViewModel
 
-
-class AccountSettingsActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAccountSettingsBinding
-    private val viewModel: AccountSettingsViewModel by viewModels()
+class ResetPasswordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResetPasswordBinding
+    private val viewModel: ResetPasswordViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAccountSettingsBinding.inflate(layoutInflater)
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
@@ -31,9 +30,8 @@ class AccountSettingsActivity : AppCompatActivity() {
             finish()
         }
 
-        binding.resetBtn.setOnClickListener {
-            val intent = Intent(this, ResetPasswordActivity::class.java)
-            startActivity(intent)
+        binding.saveBtn.setOnClickListener {
+            Toast.makeText(this, "Save button clicked", Toast.LENGTH_SHORT).show()
         }
     }
 }
