784ee433c5c38bdd326129f44f0b8b1fe43502a2
dontkeep <alsdiaryy@gmail.com>
2024-12-10 21:43:11 +0700

    Fix bug crash app(MyFirebaseMessaging).

--- app/src/main/java/com/exal/grocerease/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/exal/grocerease/service/MyFirebaseMessagingService.kt
@@ -1,12 +1,17 @@
 package com.exal.grocerease.service
 
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
 
-        Toast.makeText(this, "New token: $token", Toast.LENGTH_SHORT).show()
+        // Switch to the main thread to show the Toast
+        Handler(Looper.getMainLooper()).post {
+            Toast.makeText(this, "New token: $token", Toast.LENGTH_SHORT).show()
+        }
     }
 }
